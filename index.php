7de780024da3926d96ac871811acedca793fb7b1
affandeshmukh <69684368+affandeshmukh@users.noreply.github.com>
2021-09-12 23:51:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -21,25 +21,45 @@
   </div> 
 </div>
 
-
 <section>
 <section class="child">
 <h2>Download Windows 7 Iso</h2>
 <img src="src/win7.jpg"></br></br>
 <button class="button"><a href="https://download.microsoft.com/download/C/0/6/C067D0CD-3785-4727-898E-60DC3120BB14/7601.24214.180801-1700.win7sp1_ldr_escrow_CLIENT_PROFESSIONAL_x86FRE_en-us.iso">For 32 Bit</a></button>
 <button class="button"><a href="https://download.microsoft.com/download/0/6/3/06365375-C346-4D65-87C7-EE41F55F736B/7601.24214.180801-1700.win7sp1_ldr_escrow_CLIENT_PROFESSIONAL_x64FRE_en-us.iso">For 64Bit</a></button>
+</br>Windows 7 is a major release of the Windows NT operating system developed by Microsoft. It was released to manufacturing on July 22, 2009, and became generally available on October 22, 2009.[9] It is the successor to Windows Vista, released nearly three years earlier. It remained an operating system for use on personal computers, including home and business desktops, laptops, tablet PCs and media center PCs, and itself was replaced in November 2012 by Windows 8, the name spanning more than three years of the product. Until April 9, 2013, Windows 7 RTM provided content such as security updates, software updates, PC driver updates and technical support, after which installation of Service Pack 1 is required for users to receive support and updates. Windows 7's server counterpart, Windows Server 2008 R2, was released at the same time. The last supported version of Windows based on this operating system was released on July 1, 2011, entitled Windows Embedded POSReady 7. On January 12, 2016, Microsoft ended support for Internet Explorer versions older than Internet Explorer 11 on Windows 7.[10][11][12] Extended support ended on January 14, 2020, over ten years after the release of Windows 7, after which the operating system ceased receiving further support or security updates to most users, and all PCs that blocks Windows Update on Windows 7 versions newer than KB4499164 released in May 2019 displays a full-screen upgrade warning notification with an information page link starting from January 15, 2020. A support program is currently available for enterprises, providing security updates for Windows 7 for up to four years since the official end of life.[13] However, Windows Embedded POSReady 7, the last Windows 7 variant, continues to receive security updates until October 2021.
+
+Windows 7 was intended to be an incremental upgrade to Microsoft Windows, addressing Windows Vista's poor critical reception while maintaining hardware and software compatibility. Windows 7 continued improvements on Windows Aero (the user interface introduced in Windows Vista) with the addition of a redesigned taskbar that allows applications to be "pinned" to it, and new window management features. Other new features were added to the operating system, including libraries, the new file-sharing system HomeGroup, and support for multitouch input. A new "Action Center" interface was also added to provide an overview of system security and maintenance information, and tweaks were made to the User Account Control system to make it less intrusive. Windows 7 also shipped with updated versions of several stock applications, including Internet Explorer 8, Windows Media Player, and Windows Media Center.
+
+Unlike Vista, Windows 7 received critical acclaim, with critics considering the operating system to be a major improvement over its predecessor because of its improved performance, its more intuitive interface (with particular praise devoted to the new taskbar), fewer User Account Control popups, and other improvements made across the platform. Windows 7 was a major success for Microsoft; even before its official release, pre-order sales for the operating system on the online retailer Amazon.com had surpassed previous records. In just six months, over 100 million copies had been sold worldwide, increasing to over 630 million licenses by July 2012. By January 2018, Windows 10 surpassed Windows 7 as the most popular version of Windows worldwide;[14] and by May 2020, Windows 10 surpassed Windows 7 as the most popular version of Windows in China.[15] As of June 2021, 15.52% of traditional PCs running Windows are running Windows 7 (and thus 5% of all devices across platforms).[16] It still remains popular in countries such as Turkmenistan, China, India, and Venezuela
 </section>
+<hr>
 <section>
 <section class="child">
 <h2 >Download Windows 8 Iso</h2>
 <img src="src/win8.jpg"></br></br>
 <button class="button"><a href="https://software-download.microsoft.com/sg/Win8.1_English_x32.iso?t=dcd05dc3-d5d8-4c99-a35f-1460716f7209&e=1631342809&h=394320e59694b66c5b0d488b9811b7d1">For 32 Bit</a></button>
 <button class="button"><a href="https://software-download.microsoft.com/sg/Win8.1_English_x64.iso?t=dcd05dc3-d5d8-4c99-a35f-1460716f7209&e=1631342809&h=024ac4545bfb5a23ed8d65f37a8f4be0">For 64Bit</a></button>
-</scetion>
+Windows 8 is a major release of the Windows NT operating system developed by Microsoft. The product was released to manufacturing on August 1, 2012, and generally to retail on October 26, 2012.[6] Windows 8 was made available for download via MSDN and TechNet and available as a replacement to all Windows 7 users via Windows Update.
+
+Windows 8 introduced major changes to the operating system's platform and user interface intended to improve its user experience on tablets, where Windows was now competing with mobile operating systems, including Android and iOS.[7] In particular, these changes included a touch-optimized Windows shell based on Microsoft's "Metro" design language and the Start screen (which displays programs and dynamically updated content on a grid of tiles), a new platform for developing "apps" with an emphasis on touchscreen input, integration with online services (including the ability to synchronize apps and settings between devices), and Windows Store, an online distribution for downloading and purchasing new software, and a new keyboard shortcut for screenshots.[8] Many of these features were adoptions from Windows Phone. Windows 8 added support for USB 3.0, Advanced Format hard drives, near field communications, and cloud computing. Additional security features were introduced, such as built-in antivirus software, integration with Microsoft SmartScreen phishing filtering service and support for UEFI Secure Boot on supported devices with UEFI firmware, to prevent malware from infecting the boot process.
+
+Windows 8 was released to a mixed critical reception. Although reaction towards its performance improvements, security enhancements, and improved support for touchscreen devices was positive, the new user interface of the operating system was widely criticized for being potentially confusing and difficult to learn, especially when used with a keyboard and mouse instead of a touchscreen. Despite these shortcomings, 60 million Windows 8 licenses were sold through January 2013, a number that included both upgrades and sales to OEMs for new PCs.[9]
+
+Microsoft released Windows 8.1 in October 2013, addressing some aspects of Windows 8 that were criticized by reviewers and early adopters and incorporated additional improvements to various aspects of the operating system.[10][11] Windows 8 was ultimately succeeded by Windows 10 in July 2015. Support for IE10 on Windows Server 2012[12][13] and Windows Embedded 8 Standard[14] ended on January 31, 2020. Market share had fallen to 1.06% by October 2020.[15]
+
+In August 2019, computer experts reported that the BlueKeep security vulnerability, CVE-2019-0708, that potentially affects older unpatched Microsoft Windows versions via the program's Remote Desktop Protocol, allowing for the possibility of remote code execution, may now include related flaws, collectively named DejaBlue, affecting newer Windows versions (i.e., Windows 7 and all recent versions).[16] In addition, experts reported a Microsoft security vulnerability, CVE-2019-1162, based on legacy code involving Microsoft CTF and ctfmon (ctfmon.exe), that affects all Windows versions from the older Windows XP version to the most recent Windows 10 versions; a patch to correct the flaw is currently available
+</section>
+
 <h2>Download Windows 10 Iso</h2>
 <img src="src/win10.jpg"></br></br>
-<button class="button"><a href="https://software-download.microsoft.com/sg/Win10_21H1_English_x32.iso?t=05a60b37-f34b-4dab-bc39-9581d0ebacda&e=1631460498&h=3d560d9749fdba85b46fda489a55fc60">For 32 Bit</a></button>
+<button class="button"><a href="https://software-download.microsoft.com/sg/Win10_21H1_English_x32.iso?t=bb91e29e-38cb-4772-aafc-55fe4a75ab72&e=1631344926&h=1A87oqEPacVwgWEtQ7CRYzbgE1ZBGuV5CT">For 32 Bit</a></button>
 <button class="button"><a href="https://software-download.microsoft.com/sg/Win10_21H1_English_x64.iso?t=bb91e29e-38cb-4772-aafc-55fe4a75ab72&e=1631344926&h=6818a380c5a066224f95cf55b134c6c0">For 64Bit</a></button>
+Windows 10 is a major release of the Windows NT operating system developed by Microsoft. It is the successor to Windows 8.1, which was released nearly two years earlier, and itself was released to manufacturing on July 15, 2015, and broadly released for the general public on July 29, 2015.[17] Windows 10 was made available for download via MSDN and TechNet, as a free upgrade for retail copies of Windows 8 and Windows 8.1 users via the Windows Store, and to Windows 7 users via Windows Update. Windows 10 receives new builds on an ongoing basis, which are available at no additional cost to users, in addition to additional test builds of Windows 10, which are available to Windows Insiders. Devices in enterprise environments can receive these updates at a slower pace, or use long-term support milestones that only receive critical updates, such as security patches, over their ten-year lifespan of extended support.[18][19]
+
+Windows 10 received mostly positive reviews upon its original release. Critics praised Microsoft's decision to provide a desktop-oriented interface in line with previous versions of Windows, contrasting the tablet-oriented approach of Windows 8, although Windows 10's touch-oriented user interface mode was criticized for containing regressions upon the touch-oriented interface of its predecessor. Critics also praised the improvements to Windows 10's bundled software over Windows 8.1, Xbox Live integration, as well as the functionality and capabilities of the Cortana personal assistant and the replacement of Internet Explorer with Microsoft Edge. However, media outlets have been critical of the changes to operating system behaviors, including mandatory update installation, privacy concerns over data collection performed by the OS for Microsoft and its partners, and adware-esque tactics used to promote the operating system on its release.[20]
+
+Microsoft initially aimed to have Windows 10 installed on over one billion devices within three years of its release;[18] that goal was ultimately reached almost five years after release on March 16, 2020.[21] By January 2018, Windows 10 surpassed Windows 7 as the most popular version of Windows worldwide.[22] As of June 2021, it is estimated that 79% of Windows PCs,[22] 58% of all PCs (the rest being older Windows versions and other operating systems such as macOS and Linux), and 24% of all devices (including mobile, tablet and console)[23] are running Windows 10. On June 24, 2021, Microsoft announced Windows 10's successor, Windows 11 and it is set to release on October 5, 2021.
 
 <h2>Create Windows 10 installation media</h2>
 <img src=src/media.jpg></br></br>
@@ -49,9 +69,12 @@
 <h2>Download Windows 11 Iso</h2>
 <img src="src/win11.jpg"></br></br>
 
-<button class="button"><a href="https://software-download.microsoft.com/sg/Windows11_InsiderPreview_Client_x64_en-gb_22000.iso?t=927eb864-b5a4-4260-ad25-5bac21089250&e=1631384358&h=2cfd4325cc9363953ec5a512f09c7e52">For 64Bit</a></button>
+<button class="button"><a href="">For 32 Bit</a></button>
+<button class="button"><a href="">For 64Bit</a></button>
 
 
 <script src="js.js"></script>
 </body>
 </html>
+Note For google asense team this website not a blog this website make for any people download any windows easyliy 
+i request to you please monetize my website
