e7594f5133b50b2c1beef74553458c3f170c7c61
affandeshmukh <69684368+affandeshmukh@users.noreply.github.com>
2022-01-20 16:52:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <html>
 <head>
   <title>Download Any Windows For Free </title>
@@ -22,38 +23,65 @@
   </div> 
 </div>
 
+<center>
 <section>
 <section class="child">
+    
+        <test class="test">
+
+
 <h2>Download Windows 7 Iso</h2>
 <img src="src/win7.jpg"></br></br>
 <button class="button"><a href="https://download.microsoft.com/download/C/0/6/C067D0CD-3785-4727-898E-60DC3120BB14/7601.24214.180801-1700.win7sp1_ldr_escrow_CLIENT_PROFESSIONAL_x86FRE_en-us.iso">For 32 Bit</a></button>
-<button class="button"><a href="https://download.microsoft.com/download/0/6/3/06365375-C346-4D65-87C7-EE41F55F736B/7601.24214.180801-1700.win7sp1_ldr_escrow_CLIENT_PROFESSIONAL_x64FRE_en-us.iso">For 64Bit</a></button>
+<button class="button"><a href="https://download.microsoft.com/download/0/6/3/06365375-C346-4D65-87C7-EE41F55F736B/7601.24214.180801-1700.win7sp1_ldr_escrow_CLIENT_PROFESSIONAL_x64FRE_en-us.iso">For 64Bit</a></button></test>
+
 </section>
+
+
+
 <section>
 <section class="child">
+
 <h2 >Download Windows 8 Iso</h2>
 <img src="src/win8.jpg"></br></br>
 <button class="button"><a href="https://software-download.microsoft.com/sg/Win8.1_English_x32.iso?t=dcd05dc3-d5d8-4c99-a35f-1460716f7209&e=1631342809&h=394320e59694b66c5b0d488b9811b7d1">For 32 Bit</a></button>
 <button class="button"><a href="https://software-download.microsoft.com/sg/Win8.1_English_x64.iso?t=dcd05dc3-d5d8-4c99-a35f-1460716f7209&e=1631342809&h=024ac4545bfb5a23ed8d65f37a8f4be0">For 64Bit</a></button>
 </scetion>
+
+<section>
+<section class="child">
+    
 <h2>Download Windows 10 Iso</h2>
 <img src="src/win10.jpg"></br></br>
 <button class="button"><a href="https://software-download.microsoft.com/sg/Win10_21H1_English_x32.iso?t=bb91e29e-38cb-4772-aafc-55fe4a75ab72&e=1631344926&h=1A87oqEPacVwgWEtQ7CRYzbgE1ZBGuV5CT">For 32 Bit</a></button>
 <button class="button"><a href="https://software-download.microsoft.com/sg/Win10_21H1_English_x64.iso?t=bb91e29e-38cb-4772-aafc-55fe4a75ab72&e=1631344926&h=6818a380c5a066224f95cf55b134c6c0">For 64Bit</a></button>
+</section>
+
+
+<section>
+<section class="child">
+        <test class="test">
 
 <h2>Create Windows 10 installation media</h2>
 <img src=src/media.jpg></br></br>
 <button class="button"><a href="src/MediaCreationTool21H1.exe">Download Now</a></button>
+</section>
 
-
+<section>
+<section class="child">
+    
 <h2>Download Windows 11 Iso</h2>
 <img src="src/win11.jpg"></br></br>
 
 <button class="button"><a href="">For 32 Bit</a></button>
 <button class="button"><a href="">For 64Bit</a></button>
+</section>
 
+</center>
 
 <script src="js.js"></script>
 </body>
 </html>
+
+
 
