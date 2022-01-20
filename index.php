9b63341e99e314f1270a03e6d85833005b1d897b
affandeshmukh <69684368+affandeshmukh@users.noreply.github.com>
2022-01-20 14:50:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -57,5 +57,3 @@
 </body>
 </html>
 
-<p>TO Google Adsense Team </p><p> this website not a blog this website make for any people who want to download any windows easily </p>
-<p>I request to you please monetize my website</p>
